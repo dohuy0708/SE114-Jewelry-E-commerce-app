7d8d278debea3267f9d54121cef999cf2da6fcd0
dohuy0708 <dohuy22314@gmail.com>
2024-03-26 21:31:18 +0700

    test git

---
+++ app/src/main/java/com/example/jewelryecommerceapp/Dialogs/newDialog.java
@@ -0,0 +1,4 @@
+package com.example.jewelryecommerceapp.Dialogs;
+
+public class newDialog {
+}
---
+++ app/src/main/java/com/example/jewelryecommerceapp/Interfaces/newInterfaces.java
@@ -0,0 +1,4 @@
+package com.example.jewelryecommerceapp.Interfaces;
+
+public class newInterfaces {
+}
