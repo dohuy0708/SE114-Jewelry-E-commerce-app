6768f45e1d4065a39cd837d4afb87f39f3fefea0
huyhoang462 <22520462@gm.uit.edu.vn>
2024-04-05 14:37:04 +0700

    add admin activity

--- app/src/main/java/com/example/jewelryecommerceapp/Fragments/AdHomeFragment.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Fragments/AdHomeFragment.java
@@ -2,23 +2,13 @@ package com.example.jewelryecommerceapp.Fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.jewelryecommerceapp.Adapters.ProductAdapter;
-import com.example.jewelryecommerceapp.Adapters.TopRateAdapter;
-import com.example.jewelryecommerceapp.Adapters.TopWeekAdapter;
-import com.example.jewelryecommerceapp.Models.Product;
 import com.example.jewelryecommerceapp.R;
-
-import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -72,47 +62,5 @@ public class AdHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ad_home, container, false);
-    }
-
-    RecyclerView rc_topWeak, rc_topRate,rc_service;
-
-    TopRateAdapter topRateAdapter;
-    TopWeekAdapter topWeekAdapter;
-    ArrayList<Product> topRateList,topWeekList,serviceList;
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        rc_service=view.findViewById(R.id.serviceList);
-        serviceList = new ArrayList<>();
-
-        //myAdapter = new ProductAdapter(getContext(),myList);
-
-        topWeekList = new ArrayList<>();
-        topWeekList.add( new Product(R.drawable.demo,"Nhẫn","A12"));
-        topWeekList.add(new Product(R.drawable.demo,"Vòng cổ","A14"));
-        topWeekList.add(new Product(R.drawable.demo,"Bông tai","A26"));
-        topWeekList.add(new Product(R.drawable.demo,"Lắc tay","B88"));
-        topWeekList.add(new Product(R.drawable.demo,"Trâm","B77"));
-        rc_topWeak=view.findViewById(R.id.topWeekList);
-        topWeekAdapter=new TopWeekAdapter(getContext(),topWeekList);
-        rc_topWeak.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-        rc_topWeak.setHasFixedSize(true);
-        rc_topWeak.setAdapter(topWeekAdapter);
-
-        topRateList = new ArrayList<>();
-        topRateList.add( new Product(R.drawable.demo,"Nhẫn Vàng",5.0));
-        topRateList.add(new Product(R.drawable.demo,"Nhẫn Bạc",4.9));
-        topRateList.add(new Product(R.drawable.demo,"Nhẫn kim cương",4.9));
-        topRateList.add(new Product(R.drawable.demo,"Lắc tay",4.9));
-        topRateList.add(new Product(R.drawable.demo,"Trâm",4.8));
-        rc_topRate=view.findViewById(R.id.topRateList);
-        topRateAdapter=new TopRateAdapter(getContext(),topRateList);
-        rc_topRate.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-        rc_topRate.setHasFixedSize(true);
-        rc_topRate.setAdapter(topRateAdapter);
-
     }
 }
