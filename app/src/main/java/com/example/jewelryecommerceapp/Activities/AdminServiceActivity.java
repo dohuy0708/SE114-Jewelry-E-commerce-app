752f4c2eb4a1ef588bdbafb8a64c02656269c5d0
ToHoanghuy <22520573@gm.uit.edu.vn>
2024-05-07 10:35:06 +0700

    update-ad-service

--- app/src/main/java/com/example/jewelryecommerceapp/Activities/AdminServiceActivity.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Activities/AdminServiceActivity.java
@@ -1,9 +1,12 @@
 package com.example.jewelryecommerceapp.Activities;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -19,13 +22,18 @@ import java.util.List;
 
 public class AdminServiceActivity extends AppCompatActivity {
 
-
+    private Context context;
     private RecyclerView recyclerView;
     private Button addButton;
 
     private Button buttonServiceType;
     private AdServiceAdapter adapter;
     private List<AdService> serviceItemList;
+    private int numberOfServicesAdded = 0;
+    private TextView numberServiceTextView;
+
+    private double totalValue = 0;
+    private TextView totalValueTextView;
 
         @SuppressLint("MissingInflatedId")
         @Override
@@ -33,33 +41,31 @@ public class AdminServiceActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_service);
 
-
             recyclerView = findViewById(R.id.recyclerview2);
             addButton = findViewById(R.id.button8);
+            numberServiceTextView = findViewById(R.id.number_service);
+            totalValueTextView = findViewById(R.id.textView32);
 
             serviceItemList = new ArrayList<>();
-            adapter = new AdServiceAdapter(serviceItemList);
+            adapter = new AdServiceAdapter(context, serviceItemList, totalValueTextView);
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
             addButton.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("DefaultLocale")
                 @Override
                 public void onClick(View v) {
-                    // Tạo một ServiceItem mới hoặc tùy chỉnh theo yêu cầu của bạn
                     AdService newItem = new AdService();
-
-                    // Thêm ServiceItem mới vào danh sách dữ liệu
                     //serviceItemList.add(newItem);
-
-                    // Thông báo cho adapter về việc thay đổi trong danh sách dữ liệu
                     //adapter.notifyDataSetChanged();
                     adapter.addService(newItem);
+                    numberOfServicesAdded++;
+                    numberServiceTextView.setText(String.valueOf(numberOfServicesAdded));
+
+                    totalValueTextView.setText(String.format("%.0f VND", adapter.calculateTotalValue()));
+                    Log.d("TotalValue", "Total value: " + totalValue+ adapter.calculateTotalValue());
                 }
-
-
             });
-
-
 
 
         }
