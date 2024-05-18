510030d9ff150a4e1cad7e022e4b6fefec4a1b67
Quang Huy <22520539@gm.uit.edu.vn>
2024-05-18 18:17:55 +0700

    add customer manage adapter

--- app/src/main/java/com/example/jewelryecommerceapp/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Activities/SplashScreenActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.jewelryecommerceapp.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -16,7 +22,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent( SplashScreenActivity.this,HomeActivity.class);
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                if (user != null) {
+//                    if(Objects.equals(user.getEmail(), "huydq58422@gmail.com"))
+//                    {
+//                        Intent intent = new Intent(SplashScreenActivity.this, AdminHomeActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                    Intent intent = new Intent( SplashScreenActivity.this,HomeActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//                else{
+                  Intent intent = new Intent( SplashScreenActivity.this,HomeActivity.class);
                 startActivity(intent);
                 finish();
 
--- app/src/main/java/com/example/jewelryecommerceapp/Models/Customer.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Models/Customer.java
@@ -1,23 +1,31 @@
 package com.example.jewelryecommerceapp.Models;
 
+import android.graphics.Bitmap;
+
 public class Customer {
-    private Image avatar;
+    private Bitmap avatar;
     private int id;
     private String name;
     private String dateCreate;
-    public Customer(Image avatar,int id, String name, String dateCreate)
+    public Customer(Bitmap avatar,int id, String name, String dateCreate)
     {
         this.avatar = avatar;
         this.id = id;
         this.name = name;
         this.dateCreate = dateCreate;
     }
-    public Image getAvatar() {return avatar;};
+    public Customer(int id, String name, String dateCreate)
+    {
+        this.id = id;
+        this.name = name;
+        this.dateCreate = dateCreate;
+    }
+    public Bitmap getAvatar() {return avatar;};
     public int getId() {return id;}
     public String getName() {return name;}
     public String getDateCreate() {return dateCreate;}
 
-    public void setAvatar(Image avatar) {this.avatar = avatar;}
+    public void setAvatar(Bitmap avatar) {this.avatar = avatar;}
     public void setId(int id) {this.id = id;}
     public void setName(String name) {this.name = name;}
     public void setDateCreate(String dateCreate) {this.dateCreate = dateCreate;}
