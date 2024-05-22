26bc9d93036b9523f8f39e095e2be07117b20fa7
dohuy0708 <dohuy22314@gmail.com>
2024-05-22 17:14:07 +0700

    view order detail

--- app/src/main/java/com/example/jewelryecommerceapp/Activities/Order_detail.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Activities/Order_detail.java
@@ -1,16 +1,214 @@
 package com.example.jewelryecommerceapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.jewelryecommerceapp.Adapters.CartPurchaseAdapter;
+import com.example.jewelryecommerceapp.Adapters.OrdersAdapter;
+import com.example.jewelryecommerceapp.Models.CartItem;
+import com.example.jewelryecommerceapp.Models.Order;
 import com.example.jewelryecommerceapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Order_detail extends AppCompatActivity {
 
+
+    TextView add ,masp,status,total,voucher,totallord;
+    RecyclerView rc_product;
+    Button btn_review ;
+    ImageView back;
+    private LoadingDialog loadingDialog;
+    CartPurchaseAdapter adt;
+    ArrayList<CartItem> listpro;
+    Order orderchoose = new Order();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        loadingDialog = new LoadingDialog(Order_detail.this);
+        // khai báo UI
+        initUI();
+        // lấy intent
+        Intent myintent =getIntent();
+        String ID = myintent.getStringExtra("ID");
+        Log.d("ID", ID);
+
+
+        // init list + adapter
+        listpro=new ArrayList<>();
+        adt=new CartPurchaseAdapter(this, listpro);
+
+
+
+        // Lấy dữ liệu trên firebase
+        GetOrderFromFireBase(ID);
+
+
+        // sự kiện
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btn_review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // chuyển tới đánh giá đơn hàng
+            }
+        });
+
+
+
+
     }
+
+    private void GetOrderFromFireBase(String ID) {
+        loadingDialog.show();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userid = user.getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("Đơn hàng").child(userid);
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                {
+
+                    Order order = dataSnapshot.getValue(Order.class);
+
+                    if ( order!=null&& order.getOrderID().equals(ID))
+                    {
+
+
+                        orderchoose =order;
+                        listpro = orderchoose.getListPurchaseProduct();
+
+
+                        break;
+
+                    }
+
+
+
+                }
+                adt.notifyDataSetChanged();
+
+                SetUI();
+
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        loadingDialog.cancel();
+
+    }
+
+    private void SetUI()
+    {
+        // dịa chỉ
+        add.setText(orderchoose.getAddress().getFullName()+", sđt: "+orderchoose.getAddress().getPhoneNumber()+"\n"+orderchoose.getAddress().getStreet()+", "+orderchoose.getAddress().getWard()+", "+orderchoose.getAddress().getDistrict()+", "+orderchoose.getAddress().getProvince()+"\n"+"Ghi chú: "+orderchoose.getAddress().getDetail());
+        //masp
+        masp.setText(orderchoose.getOrderID());
+        //status
+        status.setText(orderchoose.getStatus());
+        //total
+        total.setText(formatNumber(orderchoose.getMoney()+orderchoose.getVoucher())+"");
+        // totallord
+        totallord.setText(formatNumber(orderchoose.getMoney())+"");
+        // voucher
+        voucher.setText(formatNumber(orderchoose.getVoucher())+"");
+        // button
+        if(orderchoose.getStatus().equals("Đã nhận"))
+        {
+
+            btn_review.setVisibility(View.VISIBLE);
+        }
+
+
+
+        //rcView
+        adt=new CartPurchaseAdapter(this, listpro);
+        adt.notifyDataSetChanged();
+        Log.d("Size list", listpro.size()+"");
+        rc_product.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rc_product.setHasFixedSize(true);
+        rc_product.setAdapter(adt);
+
+
+    }
+
+
+    private void initUI() {
+        add = findViewById(R.id.addd);
+        masp= findViewById(R.id.masp);
+        status= findViewById(R.id.statusdetail);
+        total = findViewById(R.id.totalsp);
+        voucher=findViewById(R.id.discountvoucher);
+        totallord = findViewById(R.id.totalord);
+
+        rc_product=findViewById(R.id.rc_product_list);
+        btn_review=findViewById(R.id.btnreview);
+        back = findViewById(R.id.btnbackkkdetail);
+    }
+
+    public static String formatNumber(int number) {
+        String strNumber = String.valueOf(number); // Chuyển đổi số thành chuỗi
+        int length = strNumber.length(); // Độ dài của chuỗi số
+
+        // Xây dựng chuỗi kết quả từ phải sang trái, thêm dấu chấm sau mỗi 3 ký tự
+        StringBuilder result = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            result.insert(0, strNumber.charAt(i));
+            if ((length - i) % 3 == 0 && i != 0) {
+                result.insert(0, ".");
+            }
+        }
+
+        return result.toString(); // Trả về chuỗi kết quả
+    }
+    public void showToastWithIcon(int icon, String message) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, null);
+
+        // Tùy chỉnh icon và văn bản trong toast
+        ImageView imageView = layout.findViewById(R.id.toast_icon);
+        imageView.setImageResource(icon); // Thay 'your_icon' bằng tên icon của bạn
+        TextView textView = layout.findViewById(R.id.toast_text);
+        textView.setText(message);
+
+        // Tạo và hiển thị toast custom
+        Toast toast = new Toast(Order_detail.this);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
 }
--- app/src/main/java/com/example/jewelryecommerceapp/Adapters/OrdersAdapter.java
+++ app/src/main/java/com/example/jewelryecommerceapp/Adapters/OrdersAdapter.java
@@ -1,6 +1,11 @@
 package com.example.jewelryecommerceapp.Adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +15,27 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.jewelryecommerceapp.Activities.LoginActivity;
+import com.example.jewelryecommerceapp.Activities.Order_detail;
+import com.example.jewelryecommerceapp.Activities.ProductDetailActivity;
 import com.example.jewelryecommerceapp.Models.Order;
 import com.example.jewelryecommerceapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.OrderViewHolder> {
     Context context;
     ArrayList<Order> ListOrder;
-    private  IClickListener mIClicklistener;
-    public interface IClickListener{
+    private IClickListener mIClicklistener;
+
+    public interface IClickListener {
         void OnClickItem(String productType, String productID);
     }
 
@@ -28,80 +44,260 @@ public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.OrderViewH
         this.ListOrder = listOrder;
     }
 
-    public OrdersAdapter(Context context, ArrayList<Order> listOrder, IClickListener listener){
+    public OrdersAdapter(Context context, ArrayList<Order> listOrder, IClickListener listener) {
         this.context = context;
         this.ListOrder = listOrder;
         this.mIClicklistener = listener;
 
     }
+
     @NonNull
     @Override
-    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int newType)
-    {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_ad_order, parent,false);
+    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int newType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_orders, parent, false);
         return new OrderViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull OrderViewHolder holder, int position)
-    {
-        Order order=ListOrder.get(position);
-        if(order==null)
+    public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
+        Order order = ListOrder.get(position);
+        if (order == null)
             return;
-        holder.orderID.setText("Mã đơn hàng: "+ order.getOrderID());
+        holder.orderID.setText("Mã đơn hàng: " + order.getOrderID());
         //holder.ordererName.setText("Tên khách hàng: "+order.getOrdererName());
-     //   holder.ordererSDT.setText("Số điện thoại: "+order.getOrdererSDT());
-      //  holder.ordererAdd.setText("Địa chỉ: "+order.getOrdererAdd());
-        holder.datee.setText("Ngày đặt hàng: "+order.getDatee());
-        holder.money.setText("Tổng tiền: "+order.getMoney());
-        holder.dtls.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //   holder.ordererSDT.setText("Số điện thoại: "+order.getOrdererSDT());
+        //  holder.ordererAdd.setText("Địa chỉ: "+order.getOrdererAdd());
+        holder.datee.setText("Ngày đặt hàng: " + order.getDatee());
+        holder.money.setText( formatNumber(order.getMoney())+ " VND");
+        holder.status.setText(order.getStatus()+"");
 
-            }
-        });
-        holder.acpt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        String status = order.getStatus();
 
-            }
-        });
+        if (status.equals("Đang xử lý")) {
+            Log.d("status", status);
+            holder.cancel.setEnabled(true);
+           // holder.cancel.setTextColor(Color.GRAY); // Ẩn Button nhưng vẫn chiếm không gian
+            holder.cancel.setVisibility(View.VISIBLE);
+            holder.finish.setVisibility(View.GONE);
+        }
+        else if (status.equals("Đang giao")){
+            holder.finish.setVisibility(View.VISIBLE);
+            holder.cancel.setEnabled(true);
+            holder.cancel.setVisibility(View.GONE);
+
+        }
+        else {
+            holder.finish.setVisibility(View.VISIBLE);
+            holder.cancel.setEnabled(false);
+            holder.cancel.setVisibility(View.GONE);
+            holder.finish.setTextColor(Color.GRAY);
+        }
+
+       // hủy đơn
         holder.cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                GetDialogCancel("Bạn chắn chắn muốn hủy đơn hàng!",order);
+
+
             }
         });
+
+       // hoàn tất đơn
+        holder.finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    GetDialogFinish("Bạn đã nhận đơn hàng thành công",order);
+            }
+        });
+
+// xem chi tiết đơn hàng
+
+        holder.detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Order_detail.class);
+                String ID = order.getOrderID();
+                intent.putExtra("ID", ID);
+                context.startActivity(intent);
+
+
+            }
+        });
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        if (ListOrder == null) {
+            return 0;
+
+        }
+        return ListOrder.size();
     }
+
 
 
     public class OrderViewHolder extends RecyclerView.ViewHolder {
         TextView orderID;
         TextView ordererName;
         TextView ordererSDT;
-        TextView ordererAdd;
+        TextView status;
         TextView datee;
         TextView money;
-        Button dtls;
-        Button acpt;
+
+        Button detail;
         Button cancel;
+        Button finish;
         public OrderViewHolder(@NonNull View view)
         {
             super(view);
             orderID=view.findViewById(R.id.ordertitletext);
-            ordererName=view.findViewById(R.id.orderernamee);
-            ordererSDT=view.findViewById(R.id.orderersdt);
-            ordererAdd=view.findViewById(R.id.ordereradd);
+            //   ordererName=view.findViewById(R.id.orderernamee);
+            // ordererSDT=view.findViewById(R.id.orderersdt);
+            // ordererAdd=view.findViewById(R.id.ordereradd);
             datee=view.findViewById(R.id.ordererdate);
-            money=view.findViewById(R.id.orderermoney);
-            dtls=view.findViewById(R.id.buttonDtls);
-            acpt=view.findViewById(R.id.btnacpt);
+            money=view.findViewById(R.id.money);
+            status = view.findViewById(R.id.status);
+            detail= view.findViewById(R.id.btndetail);
             cancel=view.findViewById(R.id.btncancel);
+            finish=view.findViewById(R.id.btnfinish);
         }
     }
+    public static String formatNumber(int number) {
+        String strNumber = String.valueOf(number); // Chuyển đổi số thành chuỗi
+        int length = strNumber.length(); // Độ dài của chuỗi số
+
+        // Xây dựng chuỗi kết quả từ phải sang trái, thêm dấu chấm sau mỗi 3 ký tự
+        StringBuilder result = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            result.insert(0, strNumber.charAt(i));
+            if ((length - i) % 3 == 0 && i != 0) {
+                result.insert(0, ".");
+            }
+        }
+
+        return result.toString(); // Trả về chuỗi kết quả
+    }
+    void GetDialogCancel(String Message, Order order)
+    {
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(context);
+        mydialog.setTitle("Thông báo!");
+        mydialog.setMessage(Message);
+        mydialog.setIcon(R.drawable.ic_alarm);
+        mydialog.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String userid = user.getUid();
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference ref = database.getReference("Đơn hàng").child(userid);
+
+
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                        {
+
+                            Order orderget = dataSnapshot.getValue(Order.class);
+
+                            if ( orderget!=null && orderget.getOrderID().equals(order.getOrderID()))
+                            {
+
+
+                            }
+
+
+
+                        }
+                        //    ordersAdapter.notifyDataSetChanged();
+
+                        //    Toast.makeText(SearchActivity.this,productList.get(0).getProductName(),Toast.LENGTH_SHORT).show();
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+        });
+        mydialog.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        mydialog.create().show();
+    }
+
+    void GetDialogFinish(String Message, Order order)
+    {
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(context);
+        mydialog.setTitle("Thông báo!");
+        mydialog.setMessage(Message);
+        mydialog.setIcon(R.drawable.ic_alarm);
+        mydialog.setPositiveButton("Xác nhận", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String userid = user.getUid();
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference ref = database.getReference("Đơn hàng").child(userid);
+
+
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren())
+                        {
+
+                            Order orderget = dataSnapshot.getValue(Order.class);
+
+                            if ( orderget!=null && orderget.getOrderID().equals(order.getOrderID()))
+                            {
+                                dataSnapshot.getRef().removeValue().addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+
+
+                                    } else {
+                                        Log.e("Delete order", "Failed to delete order", task.getException());
+                                    }
+                                });
+
+                            }
+
+
+
+                        }
+                        //    ordersAdapter.notifyDataSetChanged();
+
+                        //    Toast.makeText(SearchActivity.this,productList.get(0).getProductName(),Toast.LENGTH_SHORT).show();
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+        });
+        mydialog.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        mydialog.create().show();
+    }
+
 }
